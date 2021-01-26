82e8fd7fd00762244ae4ed7419a4cf3891bdef17
toomanydev <69650390+toomanydev@users.noreply.github.com>
2021-01-26 18:27:21 +0000

    first commit

---
+++ scripts.js
@@ -0,0 +1,7 @@
+var availableCalories;
+var calorieTarget; // the total amount of calories intended to be consumed during the day.
+var alreadyConsumed; // calories that were consumed prior to starting the timer, e.g. breakfast.
+var bedTime; // the time at which all calories will be made available.
+var startTime; // the time the "go" button was first pressed.
+var alertEnabled; // if we alert the user with a ding or alarm, etc.
+var calorieConsumptions; // list of calories consumed after starting, i.e. excluding alreadyConsumed.
