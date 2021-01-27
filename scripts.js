5a68687382c96097ca9430a9f0cf465ad4df1bd1
toomanydev <69650390+toomanydev@users.noreply.github.com>
2021-01-27 02:20:11 +0000

    renamed to breakfast, started on JS

--- scripts.js
+++ scripts.js
@@ -1,6 +1,37 @@
+// Variables
 var calorieTarget; // the total amount of calories intended to be consumed during the day.
-var alreadyConsumed; // calories that were consumed prior to starting the timer, e.g. breakfast.
+var breakfastCalories; // calories that were consumed prior to starting the timer, e.g. breakfast.
+
 var startTime; // the time the "go" button was first pressed, unless altered.
 var bedTime; // the time at which all calories will be made available.
+
 var availableCalories;
+
 var calorieConsumptions; // list of calories consumed after starting, i.e. excluding alreadyConsumed.
+
+// Main
+main();
+function main() {
+    
+}
+
+// Functions
+function getInputValues(){
+    calorieTarget = getCalorieTarget();
+    breakfastCalories = getBreakfastCalories();
+    startTime = getStartTime();
+    bedTime = getBedTime();
+}
+
+function getCalorieTarget(){
+    return document.getElementById("calorieTarget").value;
+}
+function getBreakfastCalories(){
+    return document.getElementById("breakfastCalories").value;
+}
+function getStartTime(){
+    return document.getElementById("startTime").value;
+}
+function getBedTime(){
+    return document.getElementById("bedTime").value;
+}
