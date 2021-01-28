abfd6e300021613947b3348ef075bcae1094885b
toomanydev <69650390+toomanydev@users.noreply.github.com>
2021-01-28 00:19:52 +0000

    Uses ESLint now, npm stuff

--- scripts.js
+++ scripts.js
@@ -1,87 +1,89 @@
 // Variables
-var calorieTarget; // the total amount of calories intended to be consumed during the day.
-var breakfastCalories; // calories that were consumed prior to starting the timer, e.g. breakfast.
+let calorieTarget; // the total amount of calories intended to be consumed during the day.
+let breakfastCalories; // calories that were consumed prior to starting the timer, e.g. breakfast.
 
-var startTime; // the time the "go" button was first pressed, unless altered.
-var bedTime; // the time at which all calories will be made available.
-var currentTime; 
+let startTime; // the time the "go" button was first pressed, unless altered.
+let bedTime; // the time at which all calories will be made available.
+let currentTime;
 
-var availableCalories;
+let availableCalories;
 
-var calorieConsumptions; // list of calories consumed after starting, i.e. excluding alreadyConsumed.
+let calorieConsumptions; // list of calories consumed after starting, i.e. excluding alreadyConsumed.
 
-var updateInterval = 1000;
-var updateInstance;
-var updateCurrentTimeInstance;
+const updateInterval = 1000;
+let updateInstance;
+let updateCurrentTimeInstance;
 
 // Main
-main();
 function main() {
-    setupStartTime();
-    updateCurrentTime();
-    updateCurrentTimeInstance = setInterval(updateCurrentTime,1000);
+  setupStartTime();
+  updateCurrentTime();
+  updateCurrentTimeInstance = setInterval(updateCurrentTime, 1000);
 }
+main();
 
 // Events
 function update() {
-    console.log(getStartTime());
+  console.log(getStartTime());
 }
 function updateCurrentTime() {
-    currentTime = new Date();
-    // TODO correct current time to be 1970, same data as default
-    setCurrentTime(currentTime.getHours(), currentTime.getMinutes());
+  currentTime = new Date();
+  // TODO correct current time to be 1970, same data as default
+  setCurrentTime(currentTime.getHours(), currentTime.getMinutes());
 }
-function goButton(){
-    clearInterval(updateInstance);
-    intakeInputValues();
-    update();
-    updateInstance = setInterval(update,updateInterval);
+
+// Used in HTML
+// eslint-disable-next-line no-unused-vars
+function goButton() {
+  clearInterval(updateInstance);
+  intakeInputValues();
+  update();
+  updateInstance = setInterval(update, updateInterval);
 }
 
 // Update UI
-function updateOutputValues(){
-    setCurrentTime();
-    setAvailableCalories();
+function updateOutputValues() {
+  setCurrentTime();
+  setAvailableCalories();
 }
 function setCurrentTime(hours, mins) {
-    var hoursCorrected = correctTimeDigits(hours);
-    var minsCorrected = correctTimeDigits(mins);
-    document.getElementById("currentTime").innerHTML = hoursCorrected+":"+minsCorrected;
+  const hoursCorrected = correctTimeDigits(hours);
+  const minsCorrected = correctTimeDigits(mins);
+  document.getElementById('currentTime').innerHTML = `${hoursCorrected}:${minsCorrected}`;
 }
 function setAvailableCalories() {
-    document.getElementById("calorieTarget").value = availableCalories;
+  document.getElementById('calorieTarget').value = availableCalories;
 }
 
 // Intake values from UI
-function intakeInputValues(){
-    calorieTarget = getCalorieTarget();
-    breakfastCalories = getBreakfastCalories();
-    startTime = getStartTime();
-    bedTime = getBedTime();
+function intakeInputValues() {
+  calorieTarget = getCalorieTarget();
+  breakfastCalories = getBreakfastCalories();
+  startTime = getStartTime();
+  bedTime = getBedTime();
 }
-function getCalorieTarget(){
-    return document.getElementById("calorieTarget").value;
+function getCalorieTarget() {
+  return document.getElementById('calorieTarget').value;
 }
-function getBreakfastCalories(){
-    return document.getElementById("breakfastCalories").value;
+function getBreakfastCalories() {
+  return document.getElementById('breakfastCalories').value;
 }
-function getStartTime(){
-    return document.getElementById("startTime").valueAsDate;
+function getStartTime() {
+  return document.getElementById('startTime').valueAsDate;
 }
-function getBedTime(){
-    return document.getElementById("bedTime").valueAsDate;
+function getBedTime() {
+  return document.getElementById('bedTime').valueAsDate;
 }
 
 // Setup Times
-function setupStartTime(){
-    
+function setupStartTime() {
+  // crunch all times to same day? Only actually use hours, minutes specifically?
 }
 
 // Utility
-function correctTimeDigits(number){
-    if(number < 10) {
-        return "0"+number;
-    } else {
-        return number;
-    }
+function correctTimeDigits(number) {
+  if (number < 10) {
+    return `0${number}`;
+  }
+  return number;
 }
